c8f304333a1eaf08b0ecd5eeb51391915ce796f8
chimo-chornet <chimochomet@gmail.com>
2023-11-13 22:26:48 +0100

    zona privada restringida

--- proyecto/app/vistas/privado.php
+++ proyecto/app/vistas/privado.php
@@ -1,37 +1,43 @@
 <?php
 session_start();
+
 include("../libs/bGeneral.php");
-$usuario=$_SESSION["usuario"];
-echo("Bienvenido usuario: ".$usuario."<br>");
-$puntero=fopen("../ficheros/usuarios.txt","r");
-while(!feof($puntero)){
-    $linea=fgets($puntero);
-    $separados=explode(":",$linea);
-    if($separados[0]==$usuario) {
-        if($separados[6]!=="Sin imagen") {
-            echo("<br><img src=\"$separados[6]\" alt=\"foto\" width=\"100px\"><br>");
+if(!isset($_SESSION["usuario"])){
+echo("Esta zona es exculsiva para usuarios logeados");
+
+
+}else{
+    $usuario=$_SESSION["usuario"];
+    echo("Bienvenido usuario: ".$usuario."<br>");
+    $puntero=fopen("../ficheros/usuarios.txt", "r");
+    while(!feof($puntero)) {
+        $linea=fgets($puntero);
+        $separados=explode(":", $linea);
+        if($separados[0]==$usuario) {
+            if($separados[6]!=="Sin imagen") {
+                echo("<br><img src=\"$separados[6]\" alt=\"foto\" width=\"100px\"><br>");
+            }
+            echo("<b>E-mail: </b>".$separados[2]."<br>");
+            echo("<b>Fecha de nacimiento: </b>".$separados[3]."<br>");
+            echo("<b>Lenguaje preferido: </b>".$separados[4]."<br>");
+            echo("<b>Descripción: </b>".$separados[5]."<br>");
+
         }
-        echo("<b>E-mail: </b>".$separados[2]."<br>");
-        echo("<b>Fecha de nacimiento: </b>".$separados[3]."<br>");
-        echo("<b>Lenguaje preferido: </b>".$separados[4]."<br>");
-        echo("<b>Descripción: </b>".$separados[5]."<br>");
 
     }
-
-}
-fclose($puntero);
-?>
+    fclose($puntero);
+    ?>
 <a href="../vistas/formServicio.php">Alta de servicio</a>
 <?php
-$puntero=fopen("../ficheros/servicios.txt","r");
-echo("<h1>Lista de servicios disponibles</h1>");
-echo("<ul>");
-while(!feof($puntero)){
-$linea=fgets($puntero);
-$separados=explode(":",$linea);
-echo("<li>".$separados[0]."</li>");
+    $puntero=fopen("../ficheros/servicios.txt", "r");
+    echo("<h2>Lista de servicios disponibles</h2>");
+    echo("<ul>");
+    while(!feof($puntero)) {
+        $linea=fgets($puntero);
+        $separados=explode(":", $linea);
+        echo("<li>".$separados[0]."</li>");
+    }
+    echo("</ul>");
+    fclose($puntero);
 }
-echo("</ul>");
-fclose($puntero);
-
 ?>
